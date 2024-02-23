ed8169b714935354ae5cd9ea200d3fc185a3b943
Pedro Ramirez <prlara2000@gmail.com>
2024-02-22 23:10:52 -0600

    add for loop challenges

--- test.js
+++ test.js
@@ -1,11 +1,41 @@
 // create a function that console.log 10 times 'I am a great QA Engineer'
 
-function printTenTimes(string) {
-  for (var i = 0; i < 10; i++) {
-    console.log(string);
-  }
-}
+// function printTenTimes(string) {
+//   for (var i = 0; i < 10; i++) {
+//     console.log(string);
+//   }
+// }
 
-printTenTimes('I am a good QA');
+// printTenTimes('I am a good QA');
 
-// printString10Times("Hello, world!");
+//-------------------------------------------------------------------------------
+
+// solution to sort and array of numbers in ascending order, but not using the sort method
+
+// function sortArray(arr) {
+//   let current, next, temp;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       current = arr[j];
+//       next = arr[j + 1];
+//       if (current > next) {
+//         temp = current;
+//         arr[j] = next;
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+const numbers = [1, 25, 15, 20, 5, 10];
+// console.log(sortArray(numbers));
+
+//------------------------------------------------------------------------------------------
+// use the for loop, it is not necessry to create a function!
+// challenge #1 iterate through the array numbers and console.log each number
+// challenge #2 iterate through the array numbers and console.log each number multiplied by 2
+// challenge #3 iterate through the array and find the sum of all the numbers
+// challenge #4 iterate through the array and find the number that is the largest
+// challenge #5 iterate through the array and find the number that is the smallest
+// challenge #6 iterate through the array and find and console.log the number 20
